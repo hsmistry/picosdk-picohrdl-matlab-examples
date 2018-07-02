51ebcb1a6ba4cfd8aa5ccc7e343fe46a20f1bce1
hsmistry <hsmistry@users.noreply.github.com>
2018-07-02 15:54:12 +0100

    Add PicoHRDLConstants.m

---
+++ PicoHRDLConstants.m
@@ -0,0 +1,24 @@
+%% PicoHRDLConstants 
+%
+% The PicoHRDLConstants class defines a number of constant values,
+% including those defined in the HRDL.h header file for use with examples
+% for the PicoLog High-Resolution Data Loggers.
+%
+% Copyright: © 2018 Pico Technology Ltd. See LICENSE file for terms.
+
+classdef PicoHRDLConstants 
+    
+    properties (Constant)
+        
+        HRDL_MAX_UNITS = 20;
+        
+        MODEL_ADC_20 = '20';
+        MODEL_ADC_24 = '24';
+        
+        TRUE = 1;
+        FALSE = 0;
+        
+    end
+    
+    
+end
