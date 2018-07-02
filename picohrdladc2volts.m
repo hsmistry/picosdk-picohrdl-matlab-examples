e67dcec9f6abe48a98a03eb496937b6b452f0f9b
hsmistry <hsmistry@users.noreply.github.com>
2018-07-02 16:23:41 +0100

    Update function definition.

--- picohrdladc2volts.m
+++ picohrdladc2volts.m
@@ -1,9 +1,11 @@
 function volts = picohrdladc2volts(values, range, maxValue)
-%PL1000ADC2MV Converts milliVolt value to ADC count for PicoLog 1000 series
-%   values - raw ADC value(s).
-%   range - the enHRDLRange enumeration corresponding to the voltage range
-%   in use.
-%   maxValue - maximum ADC count for the channel.
+% PICOHRDLADC2VOLTS Converts ADC values to volts for PicoLog
+% High-Resolution Data Loggers.
+%
+% values - raw ADC value(s).
+% range - the enHRDLRange enumeration corresponding to the voltage range
+% in use.
+% maxValue - maximum ADC count for the channel.
 %
 % Copyright © 2018 Pico Technology Ltd. See LICENSE file for terms.
 
