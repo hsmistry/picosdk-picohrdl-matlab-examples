8765b6e537d55dd369c01244e7497365b7c726e5
hsmistry <hsmistry@users.noreply.github.com>
2018-07-11 12:32:51 +0100

    Update block example and publish

--- examples/PicoHRDLBlockExample.m
+++ examples/PicoHRDLBlockExample.m
@@ -87,6 +87,7 @@ end
 %% Open a connection
 
 hrdlHandle = calllib('picohrdl', 'HRDLOpenUnit');
+hrdlHandle = int16(hrdlHandle);
 
 if (hrdlHandle >= 1)
     
@@ -145,7 +146,7 @@ fprintf('\n');
 
 %% Set mains noise rejection to 50 Hz
 
-[status.setMains] = calllib('picohrdl', 'HRDLSetMains', hrdlHandle, 0);
+[status.setMains] = calllib('picohrdl', 'HRDLSetMains', hrdlHandle, int16(0));
 
 %% Set channel 
 
@@ -154,7 +155,7 @@ ch1Enabled      = PicoConstants.TRUE;
 ch1Range        = picohrdlEnuminfo.enHRDLRange.HRDL_2500_MV;
 ch1SingleEnded  = PicoConstants.TRUE;
 
-[status.setAnalogInCh1] = calllib('picohrdl', 'HRDLSetAnalogInChannel', hrdlHandle, channel1, ch1Enabled, ch1Range, ch1SingleEnded);
+[status.setAnalogInCh1] = calllib('picohrdl', 'HRDLSetAnalogInChannel', hrdlHandle, int16(channel1), int16(ch1Enabled), int16(ch1Range), int16(ch1SingleEnded));
 
 %% Get minimum and maximum ADC counts available for the channel
 % Odd and even numbered channels have a different max/min ADC count.
@@ -165,7 +166,7 @@ ch1MinAdcPtr = libpointer('int32Ptr', 0);
 ch1MaxAdcPtr = libpointer('int32Ptr', 0) ;
 
 [status.getMinMaxAdcCounts] = calllib('picohrdl', 'HRDLGetMinMaxAdcCounts', hrdlHandle, ...
-    ch1MinAdcPtr, ch1MaxAdcPtr, channel1);
+    ch1MinAdcPtr, ch1MaxAdcPtr, int16(channel1));
 
 ch1MinAdc = ch1MinAdcPtr.Value;
 ch1MaxAdc = double(ch1MaxAdcPtr.Value);
@@ -185,7 +186,7 @@ if (hasDigitalPorts > 0)
         
     end
     
-    [status.setDigitalIOChannel] = calllib('picohrdl', 'HRDLSetDigitalIOChannel', hrdlHandle, directionOut, digitalOutPinState, enabledDigitalIn);
+    [status.setDigitalIOChannel] = calllib('picohrdl', 'HRDLSetDigitalIOChannel', hrdlHandle, int16(directionOut), int16(digitalOutPinState), int16(enabledDigitalIn));
     
 end
 
@@ -210,34 +211,36 @@ end
 disp(['Number of enabled channels: ', num2str(numEnabledChannels)]);
 
 %% Set the collection time interval
-% Collect data at 1 second time intervals. All conversions should take
+% Collect data at 200 millisecond time intervals. All conversions should take
 % place within the time specified. Digital inputs are instantaneous so do
 % not need to be included when calculating the time interval.
 
-sampleIntervalMs = 1000; % Milliseconds
-conversionTimeCh1 = picohrdlEnuminfo.enHRDLConversionTime.HRDL_100MS;
+sampleIntervalMs = int32(200); % Milliseconds
+conversionTime   = int16(picohrdlEnuminfo.enHRDLConversionTime.HRDL_100MS);
+
+[status.setInterval] = calllib('picohrdl', 'HRDLSetInterval', hrdlHandle, sampleIntervalMs, conversionTime);
 
 %% Collect a block of data
-% Collect data for 30 seconds
+% Collect data for 20 seconds
 
-numValuesPerChannel = 30;
+numValuesPerChannel = 100;
 method              = picohrdlEnuminfo.enBlockMethod.HRDL_BM_BLOCK;
 isReady             = 0;
 
-disp('Starting data collection...');
+fprintf('Starting data collection...');
 
-[status.run] = calllib('picohrdl', 'HRDLRun', hrdlHandle, numValuesPerChannel, method);
+[status.run] = calllib('picohrdl', 'HRDLRun', hrdlHandle, int32(numValuesPerChannel), int16(method));
 
 % Poll the driver until the device has completed data collection.
-while (isReady == 0)
+while (isReady == PicoConstants.FALSE)
     
     isReady = calllib('picohrdl', 'HRDLReady', hrdlHandle);
     fprintf('.');
-    pause(1);
+    pause(0.1);
 
 end
 
-disp('Data collection complete.');
+fprintf('\nData collection complete.\n');
 
 %% Retrieve the data
 
@@ -251,7 +254,7 @@ valuesPtr   = libpointer('int32Ptr', zeros(numEnabledChannels * numValuesPerChan
 overflow    = 0;
 overflowPtr = libpointer('int16Ptr', 0);
 
-numValuesCollectedPerChannel = calllib('picohrdl', 'HRDLGetTimesAndValues', hrdlHandle, timesPtr, valuesPtr, overflowPtr, numValuesPerChannel);
+numValuesCollectedPerChannel = calllib('picohrdl', 'HRDLGetTimesAndValues', hrdlHandle, timesPtr, valuesPtr, overflowPtr, int32(numValuesPerChannel));
 
 if (numValuesCollectedPerChannel > 0)
    
@@ -270,20 +273,23 @@ digitalData = values(1:numEnabledChannels:end);
 channel1Data = values(2:numEnabledChannels:end);
 
 % Convert channel 1 data to volts
-channel1Data = picohrdladc2volts(channel1Data, ch1Range, ch1MaxAdc);
+[channel1Data, vMaxCh1] = picohrdladc2volts(channel1Data, ch1Range, ch1MaxAdc);
 
-% Digital data is stored as bit flags - use bit shift if using I/O pins
-% 2, 3 and/or 4 as inputs.
-digitalIO1Data = bitand(digitalData, hex2dec('1'));
+% Digital data is stored as bit flags
+digitalIO1Data = bitget(digitalData, 1, 'int32');
 
 %% Plot the data
 
 figure1 = figure('Name','PicoLog High-Resolution Data Logger Example - Block Data Capture', 'NumberTitle','off');
 axes1 = axes('Parent', figure1);
 
-% Channel 1 data
-subplot(2, 1, 1)
+if (hasDigitalPorts > 0)
+    
+    subplot(2, 1, 1)
+    
+end
 
+% Channel 1 data
 plot(times, channel1Data);
 title('Voltage vs. Time')
 xlabel('Time (ms)');
@@ -292,21 +298,42 @@ ylim([-vMaxCh1 vMaxCh1]);
 legend('Ch. 1');
 grid on;
 
-% Digital I/O pin 1 data
-subplot(2, 1, 2)
+if (hasDigitalPorts > 0)
+    
+    % Digital I/O pin 1 data
+    subplot(2, 1, 2)
 
-plot(times, digitalIO1Data);
-title('Digital Input State vs. Time')
-xlabel('Time (ms)');
-ylabel('State');
-ylim([0 1]);
-legend('D1');
-grid on;
+    plot(times, digitalIO1Data);
+    title('Digital Input State vs. Time')
+    xlabel('Time (ms)');
+    ylabel('State');
+    ylim([-0.25 1.25]);
+    legend('D1');
+    grid on;
+    
+end
+
+%% Turn off digital output (ADC-24 only)
+% Set digital pin 2 to low
+
+if (hasDigitalPorts > 0)
+    
+    directionOut = picohrdlEnuminfo.enHRDLDigitalIoChannel.HRDL_DIGITAL_IO_CHANNEL_2 + picohrdlEnuminfo.enHRDLDigitalIoChannel.HRDL_DIGITAL_IO_CHANNEL_3 + picohrdlEnuminfo.enHRDLDigitalIoChannel.HRDL_DIGITAL_IO_CHANNEL_4;
+    digitalOutPinState = 0; % Set digital I/O to high, others will remain as low.
+    enabledDigitalIn = picohrdlEnuminfo.enHRDLDigitalIoChannel.HRDL_DIGITAL_IO_CHANNEL_1;
+    
+    [status.setDigitalIOChannel] = calllib('picohrdl', 'HRDLSetDigitalIOChannel', hrdlHandle, int16(directionOut), int16(digitalOutPinState), int16(enabledDigitalIn));
+    
+end
 
 %% Close the connection
 
-closeStatus = calllib('picohrdl', 'HRDLCloseUnit', hrdlHandle);
+status.closeUnit = calllib('picohrdl', 'HRDLCloseUnit', hrdlHandle);
 
 %% Unload the library
 
-unloadlibrary('picohrdl');
+if (libisloaded('picohrdl'))
+    
+    unloadlibrary('picohrdl');
+    
+end
